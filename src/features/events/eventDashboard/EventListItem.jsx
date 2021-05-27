40667c61c7c51c43ce644eed9d54e19d154ef435
saramer95 <sara_mer95@hotmail.com>
2021-05-27 16:33:35 -0400

    End of section 4

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -1,42 +1,61 @@
 import React from 'react';
-import { Icon, Item, ItemGroup, Segment, List, Button } from 'semantic-ui-react';
+import {
+  Icon,
+  Item,
+  ItemGroup,
+  Segment,
+  List,
+  Button,
+} from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
-
-export default function EventListItem({event}) {
-    return(
-        <Segment.Group>
-            <Segment>
-                <ItemGroup>
-                    <Item>
-                        <Item.Image size='tiny' circular src={event.hostPhotoURL}/>
-                        <Item.Content>
-                            <Item.Header content={event.title} />
-                            <Item.Description>
-                                Hosted by {event.hostedBy}
-                            </Item.Description>
-                        </Item.Content>
-                    </Item>
-                </ItemGroup>
-            </Segment>
-            <Segment>
-                <span>
-                    <Icon name='clock' />{event.date}
-                    <Icon name='marker' />{event.venue}
-                </span>
-            </Segment>
-            <Segment secondary>
-                <List horizontal>
-                    {event.attendees.map( attendee => (
-                        <EventListAttendee key={attendee.id} attendee={attendee} ></EventListAttendee>
-                    ))}
-                   
-                </List>
-            </Segment>
-            <Segment clearing>
-                <div>{event.description}</div>
-                <Button color='teal' floated='right' content='View'></Button>
-            </Segment>
-        </Segment.Group>
-    );  
+export default function EventListItem({ event, selectEvent, deleteEvent }) {
+  return (
+    <Segment.Group>
+      <Segment>
+        <ItemGroup>
+          <Item>
+            <Item.Image size='tiny' circular src={event.hostPhotoURL} />
+            <Item.Content>
+              <Item.Header content={event.title} />
+              <Item.Description>Hosted by {event.hostedBy}</Item.Description>
+            </Item.Content>
+          </Item>
+        </ItemGroup>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name='clock' />
+          {event.date}
+          <Icon name='marker' />
+          {event.venue}
+        </span>
+      </Segment>
+      <Segment secondary>
+        <List horizontal>
+          {event.attendees.map((attendee) => (
+            <EventListAttendee
+              key={attendee.id}
+              attendee={attendee}
+            ></EventListAttendee>
+          ))}
+        </List>
+      </Segment>
+      <Segment clearing>
+        <div>{event.description}</div>
+        <Button
+          onClick={() => deleteEvent(event.id)}
+          color='red'
+          floated='right'
+          content='Delete'
+        ></Button>
+        <Button
+          onClick={() => selectEvent(event)}
+          color='teal'
+          floated='right'
+          content='View'
+        ></Button>
+      </Segment>
+    </Segment.Group>
+  );
 }
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,32 +1,109 @@
-import React from 'react';
+import cuid from 'cuid';
+import React, { useState } from 'react';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
 
-export default function EventForm({setFormOpen}) {
-    return (
-        <Segment clearing>
-            <Header content='Create New Event'></Header>
-            <Form>
-                <Form.Field>
-                    <input type='text' placeholder='Event title'></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='Category'></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='Description'></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='City'></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='Venue'></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type='Date' placeholder='Date'></input>
-                </Form.Field>
-                <Button type='submit' floated='right' positive content='Submit' />
-                <Button onClick={ () => setFormOpen(false) }type='submit' floated='right' content='Cancel' />
-            </Form>
-        </Segment> 
-    );
+export default function EventForm({
+  setFormOpen,
+  setEvents,
+  createEvent,
+  selectedEvent,
+  updateEvent,
+}) {
+  const initialValues = selectedEvent ?? {
+    title: '',
+    category: '',
+    description: '',
+    city: '',
+    venue: '',
+    date: '',
+  };
+  const [values, setValues] = useState(initialValues);
+
+  function handleFormSubmit() {
+    selectedEvent
+      ? updateEvent({...selectedEvent, ...values})
+      : createEvent({
+          ...values,
+          id: cuid(),
+          hostedBy: 'Bob',
+          attendees: [],
+          hostPhotoURL: '/assets/user.png',
+        });
+    setFormOpen(false);
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
+  return (
+    <Segment clearing>
+      <Header
+        content={selectedEvent ? 'Edit the Event' : 'Create New Event'}
+      ></Header>
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Field>
+          <input
+            type='text'
+            placeholder='Event title'
+            name='title'
+            value={values.title}
+            onChange={(e) => handleInputChange(e)}
+          ></input>
+        </Form.Field>
+        <Form.Field>
+          <input
+            type='text'
+            placeholder='Category'
+            name='category'
+            value={values.category}
+            onChange={(e) => handleInputChange(e)}
+          ></input>
+        </Form.Field>
+        <Form.Field>
+          <input
+            type='text'
+            placeholder='Description'
+            name='description'
+            value={values.description}
+            onChange={(e) => handleInputChange(e)}
+          ></input>
+        </Form.Field>
+        <Form.Field>
+          <input
+            type='text'
+            placeholder='City'
+            name='city'
+            value={values.city}
+            onChange={(e) => handleInputChange(e)}
+          ></input>
+        </Form.Field>
+        <Form.Field>
+          <input
+            type='text'
+            placeholder='Venue'
+            name='venue'
+            value={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          ></input>
+        </Form.Field>
+        <Form.Field>
+          <input
+            type='Date'
+            placeholder='Date'
+            name='date'
+            value={values.date}
+            onChange={(e) => handleInputChange(e)}
+          ></input>
+        </Form.Field>
+        <Button type='submit' floated='right' positive content='Submit' />
+        <Button
+          onClick={() => setFormOpen(false)}
+          type='submit'
+          floated='right'
+          content='Cancel'
+        />
+      </Form>
+    </Segment>
+  );
 }
